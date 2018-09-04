01b140778fd826cc087e004723c7003591ee3685
Benjamín Ortega García <benjamin01234@hotmail.com>
2018-09-03 21:46:13 -0700

    Oh si

--- Metodos Estáticos/matematicas.cs
+++ Metodos Estáticos/matematicas.cs
@@ -10,7 +10,7 @@ namespace Metodos_Estáticos
     {
 
         public static int Potencia(int numeroBase, int exponente)
-        {
+        { 
             int resultado = 1;
             for(int i=0; i < exponente; i++)
             {
